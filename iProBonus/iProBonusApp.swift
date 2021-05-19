cf74a7b083cfc5482ba838e858fc525fb4d0565d
Mark Nagibin <myrkwill@icloud.com>
2021-05-19 21:39:11 +0300

    Initial Commit

---
+++ iProBonus/iProBonusApp.swift
@@ -0,0 +1,17 @@
+//
+//  iProBonusApp.swift
+//  iProBonus
+//
+//  Created by Mark Nagibin on 19.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct iProBonusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
